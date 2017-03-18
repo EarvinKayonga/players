6a4512ca23e79cd29bf847d5385e68681457f0f3
EarvinKayonga <demoneartin@gmail.com>
2017-03-18 16:16:31 +0100

    displaying players

--- front/src/Models.elm
+++ front/src/Models.elm
@@ -1,5 +1,20 @@
-module Models exposing (Model)
+module Models exposing (..)
 
 
 type alias Model =
+    { players : List Player
+    }
+
+initialModel : Model
+initialModel = 
+    { players = [ Player "1" "Sam" 1 ]
+    }
+
+type alias PlayerId =
     String
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
